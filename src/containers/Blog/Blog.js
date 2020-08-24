13902965bc05d0b4751697f226c280b12be7a111
Nitesh Bagle <niteshbagle43@gmail.com>
2020-08-25 00:01:32 +0530

    react router initial commits

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,11 +1,9 @@
 // import axios from 'axios';
 import axios from 'axios';
-
 import React, { Component } from 'react';
-
 import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import FullPost from '../FullPost/FullPost';
+import NewPost from '../NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -53,8 +51,16 @@ class Blog extends Component {
         }
 
         return (
-            <div>
-                <section className="Posts">
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <section>
                     {post}
                 </section>
                 <section>

203e03f276a4687f8e53546bcf36bc51d3fcfc2e
Nitesh Bagle <niteshbagle43@gmail.com>
2020-08-30 12:59:36 +0530

    guards and not found error catch

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,6 +7,9 @@ import NewPost from '../NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        auth: false
+    }
     render () {
         return (
             <div >
@@ -30,8 +33,9 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    <Route path="/new-post" component={NewPost}/>
+                    { !this.state.auth ? <Route path="/new-post" component={NewPost}/> : null }
                     <Route path="/posts" component={Posts}/>
+                    {/* <Route render={() => <h1>Not Found</h1>} /> */}
                     <Redirect from ="/" to="/posts"></Redirect>
                 </Switch>
             </div>
--- src/containers/NewPost/NewPost.js
+++ src/containers/NewPost/NewPost.js
@@ -12,6 +12,7 @@ class NewPost extends Component {
     }
 
     componentDidMount(){
+        // If auth --> this.props.history.replace('/posts');
         console.log(this.props);
     }
     postDataHandler = () => {
